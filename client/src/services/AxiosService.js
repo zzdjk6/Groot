9312aefd11855db03c7011f7c0065c6239f81629
Shenghan Chen <zzdjk6@gmail.com>
2018-08-06 02:54:16 +1200

    Web UI: use persisted query
    Web UI: use persisted query

--- client/src/services/AxiosService.js
+++ client/src/services/AxiosService.js
@@ -2,24 +2,37 @@
 
 import axios from "axios";
 import StorageService from "./StorageService";
+import persistedQuery from "../graphql/persisted_query.json";
+import compress from "graphql-query-compress";
 
 export default class AxiosService {
     static getAxiosInstance(query: *, variables: *) {
         let headers = {};
-
         const user = StorageService.readUser();
-
         if (user && user.Token) {
             headers["Authorization"] = "Bearer " + user.Token;
+        }
+
+        // webpack graphql loader will trim "query" at the beginning
+        let compressedQuery = compress(query);
+        if (!compressedQuery.startsWith("query")) {
+            compressedQuery = "query" + compressedQuery;
+        }
+
+        let data: { id?: string, query?: string, variables?: * } = {
+            query: compressedQuery,
+            variables: variables
+        };
+
+        if (persistedQuery[compressedQuery]) {
+            delete data.query;
+            data.id = persistedQuery[compressedQuery];
         }
 
         return axios.create({
             url: "/graphql",
             method: "post",
-            data: {
-                query,
-                variables
-            },
+            data: data,
             headers: headers
         });
     }
